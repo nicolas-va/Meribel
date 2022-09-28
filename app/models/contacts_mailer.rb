1f5dcc862749b1258daf00db9752b3ae9ca476fd
nicolas-va <68918779+nicolas-va@users.noreply.github.com>
2022-09-28 16:19:46 +0100

    rails new with bootstrap & simple_form

---
+++ app/models/contacts_mailer.rb
@@ -0,0 +1,19 @@
+class Contact < MailForm::Base
+  attribute :surname,   validate: true
+  attribute :name,      validate: true
+  attribute :email,     validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :starting_date, validate:true
+  attribute :end_date, validate:true
+  attribute :message
+
+
+  # Declare the e-mail headers. It accepts anything the mail method
+  # in ActionMailer accepts.
+  def headers
+    {
+      :subject => "Contact Form Inquiry",
+      :to => "nicolas.vassel@gmail.com",
+      :from => %("#{name}" <#{email}>)
+    }
+  end
+end
